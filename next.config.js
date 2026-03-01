8d7266e7e6965ee4912d51c5bd9cba81873b1cc2
Avinash <avinash.us101@gmail.com>
2026-03-01 16:22:48 +0530

    URL changed

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     // Dynamically pick destination based on environment or manual toggle
     const isProd = process.env.NODE_ENV === 'production' || process.env.USE_PROD_API === 'true';
     const apiUrl = isProd
-      ? 'https://seo-flow-ai-jj6c.onrender.com/api/v1/:path*'
+      ? 'https://autorankr-ai.onrender.com/api/v1/:path*'
       : 'http://127.0.0.1:8000/api/v1/:path*';
 
     return [
